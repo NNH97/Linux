26c689ffa8026293a68e339927d4accfd8db9214
NNH97 <nnhoai97@gmail.com>
2018-05-03 10:25:53 +0700

    02/05/2018

---
+++ bai1.sh
@@ -0,0 +1,7 @@
+echo "Today is: "; date
+echo "Hello "; echo $LOGNAME ;
+echo "Your current working directory: "; pwd 
+echo "Your home directory: ";echo $HOME ;  
+echo "Please press any key to finish:"
+read -p "Nhap Key : " k
+echo "---------Thank you very much!!"
